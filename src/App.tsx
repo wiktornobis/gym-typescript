2950217c3f587039e2f17ee5662fe6b7242d81cc
wiktor <nobis171@wp.pl>
2024-03-12 08:10:36 +0100

    add enum into nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import {SelectedPage} from "@/shared/types.ts";
+
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
     <>
       <div className="app bg-gray-20">
-        <Navbar />
+        <Navbar
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+        />
       </div>
     </>
   )
