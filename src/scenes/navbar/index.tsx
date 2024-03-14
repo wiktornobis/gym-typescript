34ef842206a0e6087ea59ee9e0340f8510603a89
wiktor <nobis171@wp.pl>
2024-03-14 21:13:38 +0100

    completed nav

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,25 +4,28 @@ import Logo from "@/assets/Logo.png"
 import Link from "@/scenes/navbar/Link.tsx";
 import {SelectedPage} from "@/shared/types.ts";
 import useMediaQuery from "@/hooks/useMediaQuery.ts";
-import {Bars3Icon} from "@heroicons/react/24/solid";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 import {useState} from "react";
 import ActionButton from "@/shared/ActionButton.tsx";
 
 type Props = {
     selectedPage: SelectedPage;
     setSelectedPage: (value: SelectedPage) => void;
+    isTopOfPage: boolean;
 }
 
-const Navbar = ({ selectedPage, setSelectedPage }:Props) => {
+
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }:Props) => {
     const flexBetween = "flex items-center justify-between";
-    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
+
     return <nav>
-            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+            <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
                 <div className={`${flexBetween} mx-auto w-5/6`}>
                     <div className={`${flexBetween} w-full gap-16`}>
                         <img alt="logo" src={Logo} />
-
                         {isAboveMediumScreens ? (
                         <div className={`${flexBetween} w-full`}>
                             <div className={`${flexBetween} gap-8 text-sm`}>
@@ -62,6 +65,36 @@ const Navbar = ({ selectedPage, setSelectedPage }:Props) => {
                     </div>
                 </div>
             </div>
+
+        {/*Mobile menu modal*/}
+        {!isAboveMediumScreens && isMenuToggled && (
+            <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+                <div className="flex justify-end p-12">
+                    <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                        <XMarkIcon className="h-6 w-6 text-gray-400" />
+                    </button>
+                </div>
+                <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                    <Link page="Home"
+                          selectedPage={selectedPage}
+                          setSelectedPage={setSelectedPage}
+                    />
+                    <Link page="Benefits"
+                          selectedPage={selectedPage}
+                          setSelectedPage={setSelectedPage}
+                    />
+                    <Link page="Our Classes"
+                          selectedPage={selectedPage}
+                          setSelectedPage={setSelectedPage}
+                    />
+                    <Link page="Contact Us"
+                          selectedPage={selectedPage}
+                          setSelectedPage={setSelectedPage}
+                    />
+
+                </div>
+            </div>
+        )}
         </nav>
 }
 
