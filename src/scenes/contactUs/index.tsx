93c31b50fb32c200831610cc1620555defe348bb
wiktor <nobis171@wp.pl>
2024-04-08 18:38:52 +0200

    add validation form

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -7,9 +7,8 @@ type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 };
 
-
 const ContactUs = ({ setSelectedPage }: Props) => {
-    const inputStyles = `w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+    const inputStyles = `w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white mt-5`;
 
     const {
         register,
@@ -22,7 +21,6 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             e.preventDefault();
         }
     }
-
 
     return (
         <section id="contactus" className="mx-auto w-5/6 pt-24 pb-24">
@@ -59,17 +57,52 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                         {errors.name && (
                             <p className="mt-1 text-primary-500">
                                 {errors.name.type === "required" && "This field is required"}
-                                {errors.name.type === "maxLength" && "This field is required"}
+                                {errors.name.type === "maxLength" && "Max length is 100 char."}
+                            </p>
+                        )}
+                        <input
+                            className={inputStyles}
+                            type="text"
+                            placeholder="EMAIL"
+                            {...register("email", {
+                                required: true,
+                                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            })}
+                        />
+                        {errors.email && (
+                            <p className="mt-1 text-primary-500">
+                                {errors.email.type === "required" && "This field is required"}
+                                {errors.email.type === "pattern" && "Invalid email address"}
+                            </p>
+                        )}
+                        <textarea
+                            className={inputStyles}
+                            placeholder="MESSAGE"
+                            rows={4}
+                            cols={50}
+                            {...register("message", {
+                                required: true,
+                                maxLength: 2000,
+                            })}
+                        />
+                        {errors.message && (
+                            <p className="mt-1 text-primary-500">
+                                {errors.message.type === "required" && "This field is required"}
+                                {errors.message.type === "maxLength" && "Max length is 2000 char."}
                             </p>
                         )}
 
+                        <button
+                            type="submit"
+                            className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+                        >
+                            SUBMIT
+                        </button>
                     </form>
                     <HText>
                         <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
                     </HText>
-
                 </motion.div>
-
             </motion.div>
         </section>
     );
